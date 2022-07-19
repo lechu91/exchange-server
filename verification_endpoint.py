789f2b8e490646d877162807df021f9c777a6780
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-07-18 23:04:06 -0700

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -39,7 +39,7 @@ def verify():
         # Check Ethereum
         # eth_account.Account.recover_message(eth_encoded_msg, sig.hex()) == pk:
         
-        if eth_account.Account.recover_message(a_eth_encoded_msg, sig.hex()) == pk:
+        if eth_account.Account.recover_message(eth_encoded_msg, signature=a_eth_sig_obj.signature.hex()) == pk:
             result = True
         else:
             result = False
