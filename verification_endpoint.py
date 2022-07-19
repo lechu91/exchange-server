3fb8f6f30cbcc76110078863ef29ab3f882d4fff
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-07-18 22:41:08 -0700

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -12,17 +12,17 @@ app.url_map.strict_slashes = False
 def verify():
     content = request.get_json(silent=True)
     
-    sig = content['sig']
-    message = content['payload']['message']
-    pk = content['payload']['pk']
+    sig = json.dumps(content['sig'])
+    message = json.dumps(content['payload']['message'])
+    pk = json.dumps(content['payload']['pk'])
     payload = json.dumps(content['payload'])
     eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
   
     if content['payload']['platform'] == 'Ethereum':
        
         # Check Ethereum
-#         eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
-        if eth_account.Account.recover_message(payload, sig.hex()) == pk:
+        eth_encoded_msg = eth_account.messages.encode_defunct(text=payload)
+        if eth_account.Account.recover_message(eth_encoded_msg, sig.hex()) == pk:
             result = True
         else:
             result = False
