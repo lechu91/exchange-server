c96f0a611fa3cbf3f72ce9302366b5bd8466fa9a
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-07-18 21:19:03 -0700

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -12,17 +12,20 @@ app.url_map.strict_slashes = False
 def verify():
     content = request.get_json(silent=True)
     
-    message = json.load(content)
+    if True:
+        result = True
+    
+#     message = json.load(content)
 
-    if True:#message['payload']['platform'] == 'Ethereum':
-        # Do Ethereum validation
-        result = True #Should only be true if signature validates
-    elif False: #message['payload']['platform'] == 'Algorand':
-        # Do Algorand validation
-        result = False
-    else:
-        #Check if signature is valid
-        result = True #Should only be true if signature validates
+#     if True:#message['payload']['platform'] == 'Ethereum':
+#         # Do Ethereum validation
+#         result = True #Should only be true if signature validates
+#     elif False: #message['payload']['platform'] == 'Algorand':
+#         # Do Algorand validation
+#         result = False
+#     else:
+#         #Check if signature is valid
+#         result = True #Should only be true if signature validates
 
     return jsonify(result)
 
