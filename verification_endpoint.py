9f8c913af3d98cc42001df6e55d0d65713e3a49b
lechu91 <51946227+lechu91@users.noreply.github.com>
2022-07-18 21:05:12 -0700

    Update verification_endpoint.py

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -11,7 +11,6 @@ app.url_map.strict_slashes = False
 @app.route('/verify', methods=['GET','POST'])
 def verify():
     content = request.get_json(silent=True)
-
     
     message = json.load(content)
 
@@ -21,9 +20,10 @@ def verify():
     elif message['payload']['platform'] == 'Algorand':
         # Do Algorand validation
         result = False
+    else:
+        #Check if signature is valid
+        result = True #Should only be true if signature validates
 
-    #Check if signature is valid
-#    result = True #Should only be true if signature validates
     return jsonify(result)
 
 if __name__ == '__main__':
